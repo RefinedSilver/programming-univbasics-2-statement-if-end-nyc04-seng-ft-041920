021fdb5cdbc0af380ebb1d2da79be9c3764cc4db
Ian McKenzie <imckenzie777@gmail.com>
2020-04-24 05:50:31 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -5,7 +5,7 @@ end
 if false
   puts "This code does not run. You will not see this in IRB"
 end
-run_code_inside true
+run_code_inside = true
 puts "Code before if...end"
 if run_code_inside
   puts "code inside"
